008b01012372be2a4b45b14bd022a98b5b31bc46
diego <damaya@aplyca.com>
2019-04-29 19:32:41 -0500

    Add logic to check when cluster enabled

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
 output "endpoint" {
-  value = "${aws_db_instance.this.0.endpoint}"
+  #value = "${aws_db_instance.this.0.endpoint}"
+  value = ["${compact(concat(aws_rds_cluster_instance.this.*.endpoint, aws_db_instance.this.*.endpoint))}"]
 }
 output "address" {
-  value = "${aws_db_instance.this.0.address}"
+  #value = "${aws_db_instance.this.0.address}"
+  value = "${element(compact(concat(list(var.cluster), aws_db_instance.this.*.address)), 0)}"
 }
